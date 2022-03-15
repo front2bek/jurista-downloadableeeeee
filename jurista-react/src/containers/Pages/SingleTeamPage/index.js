af1018eaecbad670c6051807af70bb2122777825
front2bek <front2bek@gmail.com>
2022-03-15 22:19:19 +0100

    obrisane neophodne stvari sa stranice

--- jurista-react/src/containers/Pages/SingleTeamPage/index.js
+++ jurista-react/src/containers/Pages/SingleTeamPage/index.js
@@ -10,13 +10,7 @@ import { useTranslation } from "react-i18next";
 import { Route, useParams } from "react-router";
 
 // images
-import breadcumb from "../../../images/breadcumb/ratnica2.jpg";
-import pictureBircevic from "../../../images/expert/rsz_bircevic.jpg";
-import pictureCakarevic from "../../../images/expert/rsz_cakarevic.jpg";
-import pictureGagic from "../../../images/expert/rsz_gagic.jpg";
-import pictureMusli from "../../../images/expert/rsz_musli.jpg";
-import pictureVrabac from "../../../images/expert/rsz_vrabac.jpg";
-import pictureZagorac from "../../../images/expert/rsz_zagorac.jpg";
+
 import Bircevic from './bircevic';
 import Cakarevic from './cakarevic';
 import Gagic from './gagic';
@@ -26,54 +20,6 @@ import Zagorac from './zagorac';
 
 import "./style.scss";
 
-const breadcumbMenu = [
-  { name: "Home", route: "/" },
-  { name: "Attorneys", route: "/attorneys" },
-  { name: "Attorneys Details" },
-];
-
-const teamContentsBircevic = [
-  "KK Div Basket",
-  "KK Vizura",
-  "KK Radnicki",
-  "KK Partizan",
-];
-const teamContentsCakarevic = [
-  "KK Div Basket",
-  "KK Vizura",
-  "KK Radnicki",
-  "KK Partizan",
-];
-const teamContentsGagic = [
-  "KK Div Basket",
-  "KK Vizura",
-  "KK Radnicki",
-  "KK Partizan",
-];
-const teamContentsMusli = [
-  "KK Div Basket",
-  "KK Vizura",
-  "KK Radnicki",
-  "KK Partizan",
-];
-const teamContentsVrabac = [
-  "KK Div Basket",
-  "KK Vizura",
-  "KK Radnicki",
-  "KK Partizan",
-];
-const teamContentsZagorac = [
-  "KK Div Basket",
-  "KK Vizura",
-  "KK Radnicki",
-  "KK Partizan",
-];
-
-// const portfolioItem = [
-//     { images: portfolio1, title: 'General Service', subtitle: 'Corporate' },
-//     { images: portfolio2, title: 'Personal Issue', subtitle: 'General' },
-//     { images: portfolio3, title: 'Business Accounting', subtitle: 'Business' },
-// ]
 
 const SingleTeamPage = () => {
   const { t } = useTranslation();
@@ -98,7 +44,7 @@ const renderAdvokat =(param) => {
         case 'vrabac':
             return <Vrabac/>;
             default:
-            return <Vrabac/>;
+            return <Bircevic/>;
     }
 }
 
